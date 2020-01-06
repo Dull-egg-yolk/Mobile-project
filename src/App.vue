cf5f69b1ed3746cd04c60a7523546b18c38d78a3
egg <1156956096@qq.comm>
2020-01-06 18:41:41 +0800

    文件引入

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,24 @@
 <template>
   <div id="app">
-
+    <div class="icon">
+      <i class="iconfont icon-credit-level"></i>
+      <i class="iconfont icon-copy"></i>
+    </div>
+    <div class="vant">
+      <van-icon name="chat-o" />
+      <van-icon name="https://b.yzcdn.cn/vant/icon-demo-1126.png" />
+    </div>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    <van-cell-group>
+      <van-cell title="单元格" value="内容" />
+      <van-cell title="单元格" value="内容" label="测试测试" />
+    </van-cell-group>
   </div>
 </template>
 
 <style lang="less">
-
 </style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+// 引入vant
+import './utils/register-vant'
+// 引入rem适配
+import 'amfe-flexible'
+// 引入全局css
+import './styles/index.less'
 Vue.config.productionTip = false
 
 new Vue({
