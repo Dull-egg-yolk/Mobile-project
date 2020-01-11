3db96811bc5e526841688d4acef0935ad6f3dcbc
egg <1156956096@qq.comm>
2020-01-11 15:33:27 +0800

    tab-bar 配置路由

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,33 @@ const routes = [
   {
     path: '/login',
     component: () => import('../views/login')
+  },
+  {
+    path: '/',
+    component: () => import('../views/tabbar'),
+    // 子路由
+    children: [
+      {
+        path: '/qa',
+        component: () => import('../views/qa')
+      },
+
+      {
+        path: '/video',
+        component: () => import('../views/video')
+      },
+      {
+        path: '/home',
+        component: () => import('../views/home')
+      },
+      {
+        path: '/my',
+        component: () => import('../views/my')
+      }
+
+    ]
   }
+
 ]
 
 const router = new VueRouter({
---
+++ src/views/tabbar/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container-tabbar">
+
+    <router-view></router-view>
+    <van-tabbar route>
+      <van-tabbar-item icon="home-o" to="/home">首页</van-tabbar-item>
+      <van-tabbar-item icon="search" to="/qa">问答</van-tabbar-item>
+      <van-tabbar-item icon="video-o" to="/video">视频</van-tabbar-item>
+      <van-tabbar-item icon="user-o" to="/my">我的</van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
